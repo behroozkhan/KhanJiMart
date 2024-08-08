a8c3655ba84f4727a6313d7451d8325eada98fd0
behrooz khan <kbalochkhan67@gmail.com>
2024-08-08 17:56:35 +0500

    Auth Validation for phone

--- KhanJiMart/src/schemas/AuthValidations.js
+++ KhanJiMart/src/schemas/AuthValidations.js
@@ -1,7 +1,7 @@
 
 import * as Yup from 'yup';
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-
+const phoneRegex = /^\+\d{1,3}\d{9,12}$/;
 
 // Login Validation Schema
 const LoginSchema = Yup.object().shape({
@@ -20,7 +20,7 @@ const LoginSchema = Yup.object().shape({
     name: Yup.string()
         .min(3)
         .max(100)
-        .required()
+        .required("Name Is Feild")
         ,
     email: Yup.string().matches(emailRegex, 'Invalid email format').email('Invalid email').required('Email Is Required'),
     password: Yup.string()
@@ -30,6 +30,9 @@ const LoginSchema = Yup.object().shape({
       'Password must include at least one uppercase letter, one lowercase letter, and one number'
     )
     .required('Password is required'),
+    // phone: Yup.string()
+    // .matches(phoneRegex, 'Invalid phone number format. Example: +923332846693')
+    // .required('Phone number is required')
   });
 
 
