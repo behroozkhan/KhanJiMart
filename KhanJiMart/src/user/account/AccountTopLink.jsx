50f0990631f9a898763b17470ba48abd54f39bbc
behrooz khan <kbalochkhan67@gmail.com>
2024-08-08 17:57:38 +0500

    Account Top Link For getting user through redux app

--- KhanJiMart/src/user/account/AccountTopLink.jsx
+++ KhanJiMart/src/user/account/AccountTopLink.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const AccountTopLink = () => {
-  // const [user, setUser] = useState(
-  //   JSON.parse(localStorage.getItem("userData"))
-  // );
+  const { user } = useSelector((state) => state.auth);
+  const userName = user?.data?.name || "User";
 
-
+  
 
   return (
     <div>
@@ -21,7 +21,7 @@ const AccountTopLink = () => {
         <div className="flex gap-1">
             <h3>Welcome!</h3>
             {/* <h4>{user?.message?.user?.name}</h4> */}
-            <h4>behrooz Khan</h4>
+            <h4>{userName}</h4>
         </div>
 
       </div>
