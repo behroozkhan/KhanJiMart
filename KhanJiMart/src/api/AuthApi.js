efac71a1601ba9068c2920c80152bc28feb13e75
behrooz khan <kbalochkhan67@gmail.com>
2024-08-08 17:48:29 +0500

    Add AuthApi.js file for handling authentication-related API requests

--- KhanJiMart/src/api/AuthApi.js
+++ KhanJiMart/src/api/AuthApi.js
@@ -2,15 +2,42 @@ import axios from 'axios';
 
 // Setting up Base URL
 export const AuthApi = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'http://localhost:8000/api',
 });
 
 
 // Add a logout method
 AuthApi.logout = (token) => {
-  return AuthApi.post("users/logout", null, {
+  console.log('token11-->',token);
+  return AuthApi.post("user/logout", null, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      
+    },
+  });
+};
+
+
+// Method for fetching current user data using token
+AuthApi.CurrentUserDataInfo = (token) => {
+  console.log('Fetching user data with token:', token);
+  return AuthApi.get("user/current-user", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 };
+
+
+
+// Method for fetching current user data using token
+AuthApi.ChangeCurrentUserInfo = (token) => {
+  console.log('Fetching user data with token:', token);
+  return AuthApi.put("user/update-user", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+
